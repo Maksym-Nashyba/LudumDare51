442764efafa321bf7aac997b1764004cceeb5b63
Maksym-Nashyba <94983728+Maksym-Nashyba@users.noreply.github.com>
2022-10-03 01:26:38 +0200

    basic krisa + tiny fixes

---
+++ Assets/Scripts/NPCs/AI/RatNPC.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NPCs.AI
+{
+    public class RatNPC : LivingNPC
+    {
+        protected override void Start()
+        {
+            base.Start();
+            Detector.Detected += OnDetected;
+        }
+
+        private void OnDetected(SuspiciousObject obj)
+        {
+            switch (obj.Type)
+            {
+                case SuspiciousObject.Types.Parasite:
+                    ChangeState(new RunForLifeState(obj.transform));
+                    break;
+                case SuspiciousObject.Types.Corpse:
+                    break;
+                case SuspiciousObject.Types.Blood:
+                    break;
+                case SuspiciousObject.Types.HiddenParasite:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
