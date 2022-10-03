19097805dc16da62024bb8ebf8fe2d0e1951e222
Maksym-Nashyba <94983728+Maksym-Nashyba@users.noreply.github.com>
2022-10-03 22:42:45 +0200

    ...

--- Assets/Scripts/OperatorWork/CameraMover.cs
+++ Assets/Scripts/OperatorWork/CameraMover.cs
@@ -5,24 +5,29 @@ namespace OperatorWork
 {
     public class CameraMover : MonoBehaviour
     {
+        [SerializeField] private Vector3 _offset;
         private Transform _currentTarget;
         private Transform _cameraTransform;
+        private Transform _playerFollower;
 
         private void Awake()
         {
             _cameraTransform = ServiceLocator.Camera.transform;
+            _playerFollower = new GameObject().transform;
         }
 
         private void Update()
         {
-            if (_currentTarget is not null)
+            _playerFollower.position = ServiceLocator.PlayerInstance.transform.position + _offset;
+            if(ServiceLocator.GameLoop.IsPlayerControlled)
+            {
+                _cameraTransform.position =
+                    Vector3.Lerp(_cameraTransform.position, _playerFollower.position, 2f * Time.deltaTime);
+            }
+            else if (_currentTarget is not null)
             {
                 _cameraTransform.position =
                     Vector3.Lerp(_cameraTransform.position, _currentTarget.position, 2f * Time.deltaTime);   
-            }
-            else
-            {
-                
             }
         }
 
