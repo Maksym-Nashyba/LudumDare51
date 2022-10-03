8bf55794e68b60010e31b978b49851203e9fe431
Maksym-Nashyba <94983728+Maksym-Nashyba@users.noreply.github.com>
2022-10-03 03:04:01 +0200

    Infection animation for guard

---
+++ Assets/Scripts/NPCs/Animations/GuardAnimations.cs
@@ -0,0 +1,15 @@
+﻿namespace NPCs
+{
+    public class GuardAnimations : NPCAnimations
+    {
+        public override void PlayStartInfection()
+        {
+            GFXAnimator.Play("GuardStartInfection");
+        }
+
+        public override void PlayDeath(LivingNPC.DeathCauses cause)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
