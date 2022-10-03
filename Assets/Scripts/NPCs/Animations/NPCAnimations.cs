304f3487f3c064e3be6a016763b25384d21324c5
Maksym-Nashyba <94983728+Maksym-Nashyba@users.noreply.github.com>
2022-10-03 14:27:04 +0200

    Walk animations

--- Assets/Scripts/NPCs/Animations/NPCAnimations.cs
+++ Assets/Scripts/NPCs/Animations/NPCAnimations.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace NPCs
 {
@@ -6,11 +7,29 @@ namespace NPCs
     {
         [SerializeField] protected Animator GFXAnimator;
         [SerializeField] protected GameObject GFXObject;
+        protected Transform Transform;
+        private Vector3 _previousPosition;
+
+        private void Awake()
+        {
+            Transform = GetComponent<Transform>();
+        }
+
+        private void Start()
+        {
+            _previousPosition = Transform.position;
+        }
 
         public abstract void PlayStartInfection();
 
         public abstract void PlayDeath(LivingNPC.DeathCauses cause);
 
         public abstract void PlayInteractAnimation(Vector3 target);
+
+        private void FixedUpdate()
+        {
+            GFXAnimator.SetFloat("Speed", (_previousPosition-Transform.position).magnitude);
+            _previousPosition = Transform.position;
+        }
     }
 }
